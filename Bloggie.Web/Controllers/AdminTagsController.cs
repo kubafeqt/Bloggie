08e8fe915544f77908cd763ec6ec9c4252734c3f
Kubaf Jedoma <bukajcz.kubaf@seznam.cz>
2025-04-24 12:23:05 +0200

    Edit Tag, Delete Tag

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -48,5 +48,75 @@ namespace Bloggie.Web.Controllers
          return View(tags);
       }
 
+      [HttpGet]
+      public IActionResult Edit(Guid id)
+      {
+         // 1st method
+         //var tag = bloggieDbContext.Tags.Find(id);
+
+         // 2nd Method
+         var tag = bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+         if (tag != null)
+         {
+            var editTagRequest = new EditTagRequest
+            {
+               Id = tag.Id,
+               Name = tag.Name,
+               DisplayName = tag.DisplayName
+            };
+
+
+            return View(editTagRequest);
+         }
+
+         return View(null);
+      }
+
+      [HttpPost]
+      public IActionResult Edit(EditTagRequest editTagRequest)
+      {
+         var tag = new Tag
+         {
+            Id = editTagRequest.Id,
+            Name = editTagRequest.Name,
+            DisplayName = editTagRequest.DisplayName
+         };
+
+         var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+
+         if (existingTag != null)
+         {
+            existingTag.Name = tag.Name;
+            existingTag.DisplayName = tag.DisplayName;
+
+            // save changes
+            bloggieDbContext.SaveChanges();
+            //return RedirectToAction("List");
+            // Show success notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+         }
+         // Show error notification
+         return RedirectToAction("Edit", new { id = editTagRequest.Id });
+      }
+
+      [HttpPost]
+      public IActionResult Delete(EditTagRequest editTagRequest)
+      {
+         var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+         if (tag != null)
+         {
+            bloggieDbContext.Tags.Remove(tag);
+            bloggieDbContext.SaveChanges();
+
+            // Show a success notification
+            return RedirectToAction("List");
+         }
+
+         // Show an error notification  
+         return RedirectToAction("Edit", new { id = editTagRequest.Id });
+      }
+
    }
 }
